bb1e572688e946268c9a3d21d2f3b1d35c4011dc
unknown <newjincar33@gmail.com>
2021-01-17 22:10:52 +0900

    first commit

---
+++ True Delay/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+template <class Rep, class Period>
+void delay(const std::chrono::duration<Rep, Period>& delay_duration) {
+	bool use_sleep = true;
+	const auto start = std::chrono::high_resolution_clock::now();
+	for (;;) {
+		const auto time_elapsed = std::chrono::high_resolution_clock::now() - start;
+		if (delay_duration <= time_elapsed) {
+			return;
+		}
+		if (use_sleep == true) {
+			if (delay_duration - time_elapsed < 100ms) {
+				use_sleep = false;
+			}
+			else {
+				std::this_thread::sleep_for(1ms);
+			}
+		}
+	}
+}
+
+int main() {
+	auto start = chrono::high_resolution_clock::now();
+
+	delay(1s);
+
+	auto end = chrono::high_resolution_clock::now();
+	auto elapsed = chrono::duration<double, milli>(end - start).count();
+
+	cout << fixed << elapsed << "ms" << endl;
+
+	return 0;
+}
